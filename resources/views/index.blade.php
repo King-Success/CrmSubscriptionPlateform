536932b03a80d275cecfed65e0b553f08aaa0306
King-Success <code4kings@gmail.com>
2018-07-21 22:31:16 +0100

    fixed image display issue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -94,7 +94,7 @@
     <!-- Navbar Start-->
     <header class="nav-holder make-sticky">
         <div id="navbar" role="navigation" class="navbar navbar-expand-lg">
-            <div class="container"><a href="index.html" class="navbar-brand home"><img src="img/logo.png" alt="Universal logo" class="d-none d-md-inline-block"><img src="img/logo-small.png" alt="Universal logo" class="d-inline-block d-md-none"><span class="sr-only">Universal - go to homepage</span></a>
+            <div class="container"><a href="index.html" class="navbar-brand home"><img src="{{asset('img/logo.png')}}" alt="Universal logo" class="d-none d-md-inline-block"><img src="img/logo-small.png" alt="Universal logo" class="d-inline-block d-md-none"><span class="sr-only">Universal - go to homepage</span></a>
                 <button type="button" data-toggle="collapse" data-target="#navigation" class="navbar-toggler btn-template-outlined"><span class="sr-only">Toggle navigation</span><i class="fa fa-align-justify"></i></button>
                 <div id="navigation" class="navbar-collapse collapse">
                     <ul class="nav navbar-nav ml-auto">
@@ -133,7 +133,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-6 mb-small"><img src="img/template-mac.png" alt="..." class="img-fluid"></div>
+                    <div class="col-md-6 mb-small"><img src="{{asset('img/template-mac.png')}}" alt="..." class="img-fluid"></div>
                     <div class="col-md-6 text-center-sm">
                         <p class="text-uppercase text-bold">Business. Corporate. Agency.<br>Portfolio. Blog. E-commerce.<br>We have covered everything.</p>
                         <p class="no-letter-spacing">See our current packages comparison<br>to choose the right one for you.</p>
